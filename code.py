2290c18ad2708ed114dc3cc5b7af63d16ce83039
Chong Sun <sonshicyu@gmail.com>
2018-07-15 17:11:17 -0500

    Create the two class data pointers

--- code.py
+++ code.py
@@ -7,13 +7,18 @@ next(csvFile, None)
 dataFile = csv.reader(csvFile, delimiter='\t') 
 dataDim = 166
 sampleSize = 80
-group = [] 
+totalGroup = 2
+groupNum = [] 
 data = []
 
 for row in dataFile:
     data.append(map(float, row[2:-1]))
-    group.append(int(row[-1]))     
+    groupNum.append(int(row[-1]))     
 secondGroupStart = group.index(0)
 secondGroupEnd = secondGroupStart + sampleSize 
 data = np.concatenate((data[0:sampleSize][:], data[secondGroupStart:secondGroupEnd][:]), axis=0)
-group = sampleSize*[1] + sampleSize*[0]
+groupNum = sampleSize*[1] + sampleSize*[0]
+group1 = data[0:sampleSize]
+group2 = data[sampleSize:0]
+index = list(range(totalGroup*sampleSize)) 
+
